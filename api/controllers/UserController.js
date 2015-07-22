0088a332383befde25e72b7b8170086d5ee56176
Corentin Desfarges <corentin.desfarges.dev@gmail.com>
2015-07-23 00:24:03 +0200

    Removed tabs in controllers/UserController.js

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -8,70 +8,70 @@ var bcrypt = require('bcrypt');
 
 module.exports = {
 
-	login: function (req, res) {
-		User.findOne({
+  login: function (req, res) {
+    User.findOne({
       email: req.param('email')
     }, function (err, foundUser) {
       if (err) {
-				return res.negotiate(err);
-			}
+        return res.negotiate(err);
+      }
       else if (!foundUser) {
-				sails.log.debug("No user matching " + req.param('email') + ".");
-				return res.send(404);
-			}
-			else {
-				sails.log.debug("Found user " + foundUser.email + ".");
-				if(bcrypt.compareSync(req.param('password'), foundUser.password)) {
-					req.session.userId = foundUser.id;
-					req.session.authenticated = true;
-					sails.log.debug(foundUser.email + " credentials are valid.");
-					return res.send(200);
-				}
-				else {
-					sails.log.debug(foundUser.email + " credentials are invalid.");
-					return res.send(404);
-				}
-			}
-		});
-	},
+        sails.log.debug("No user matching " + req.param('email') + ".");
+        return res.send(404);
+      }
+      else {
+        sails.log.debug("Found user " + foundUser.email + ".");
+        if(bcrypt.compareSync(req.param('password'), foundUser.password)) {
+          req.session.userId = foundUser.id;
+          req.session.authenticated = true;
+          sails.log.debug(foundUser.email + " credentials are valid.");
+          return res.send(200);
+        }
+        else {
+          sails.log.debug(foundUser.email + " credentials are invalid.");
+          return res.send(404);
+        }
+      }
+    });
+  },
 
-	logout: function (req, res) {
+  logout: function (req, res) {
     User.findOne(req.session.userId, function (err, foundUser) {
       if (err) {
-				return res.negotiate(err);
-			}
-			// User does no longer exist
-      else if (!foundUser) {
-				sails.log.debug("User " + req.session.userId + " does no longer exist.");
+        return res.negotiate(err);
       }
-			// Login out
-			sails.log.debug("User " + (foundUser?foundUser.email:req.session.userId) + " logged out.");
+      // User does no longer exist
+      else if (!foundUser) {
+        sails.log.debug("User " + req.session.userId + " does no longer exist.");
+      }
+      // Login out
+      sails.log.debug("User " + (foundUser?foundUser.email:req.session.userId) + " logged out.");
       req.session.userId = null;
-			req.session.authenticated = null;
+      req.session.authenticated = null;
       return res.send(200);
     });
-	},
+  },
 
-	signup: function (req, res) {
-		// Creating new User
-		User.create({
-			firstname: req.param('firstname'),
-			name: req.param('name'),
-			birthdate: req.param('birthdate'),
-			sex: req.param('sex'),
-			password: req.param('password'),
-			email: req.param('email'),
-			phoneNumber: req.param('phoneNumber')
+  signup: function (req, res) {
+    // Creating new User
+    User.create({
+      firstname: req.param('firstname'),
+      name: req.param('name'),
+      birthdate: req.param('birthdate'),
+      sex: req.param('sex'),
+      password: req.param('password'),
+      email: req.param('email'),
+      phoneNumber: req.param('phoneNumber')
     }, function (err, newUser) {
       if (err) {
-				return res.negotiate(err);
+        return res.negotiate(err);
       }
-			else {
-				req.session.userId = newUser.id;
-				req.session.authenticated = true;
-				sails.log.debug("User " + req.param('email') + " signed up and logged in.");
-				return res.send(200);
-			}
+      else {
+        req.session.userId = newUser.id;
+        req.session.authenticated = true;
+        sails.log.debug("User " + req.param('email') + " signed up and logged in.");
+        return res.send(200);
+      }
     });
-	}
+  }
 };
