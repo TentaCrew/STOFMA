8d42df0e2dd751a4a73ef70a2ab88031752ffa4d
Frédéric Woelffel <woelffel.f@gmail.com>
2015-07-28 23:42:30 +0200

    Added a listener on sails lift event TODO : inject fake data for development purpose

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -11,6 +11,11 @@
 
 module.exports.bootstrap = function(cb) {
 
+  sails.on('lifted', function() {
+    if('development' === sails.config.environment) {
+      // TODO Inject fake data
+    }
+  });
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
   cb();
